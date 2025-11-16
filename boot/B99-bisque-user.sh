2034b5a61fcfdca5c5c52673fe055ca22915fdf1
Wahid Sadique Koly <wskoly.bp@gmail.com>
2025-11-16 12:00:54 +0600

    Updated some configs to resolve the file upload issue

--- boot/B99-bisque-user.sh
+++ boot/B99-bisque-user.sh
@@ -19,6 +19,12 @@ mkdir -p /run/bisque/data /run/bisque/local
 chown $BISQUE_USER /run/bisque  /run/bisque/data /run/bisque/local /run/bisque/data/{server_cache,uploads}
 chown $BISQUE_USER -R /source/modules
 
+# Fix sqlite database permissions if it exists
+if [ -f /source/bisque.db ]; then
+    chown $BISQUE_USER:$BISQUE_USER /source/bisque.db
+    chmod 664 /source/bisque.db
+fi
+
 rsync -a /source/public/ /usr/lib/bisque/lib/python3.10/site-packages/bq/core/public/
 chown $BISQUE_USER -R /usr/lib/bisque/lib/python3.10/site-packages/bq/core/public
 # change all except data dir
